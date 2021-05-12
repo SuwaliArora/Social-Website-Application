1375133dfcb9af928418d558c1877e2decaf314a
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-12 13:55:19 +0530

    Updated urls.py
    Added URL pattern for dashboard

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     #LoginView LogoutView view of Django's authentication framework
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
