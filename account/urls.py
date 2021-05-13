15af437a4d0fae7227e02ed0b2cd6f9ddc7d1800
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-13 13:14:58 +0530

    Updated urls.py
    The	PasswordChangeView view	will	handle	the	form to	change	the
    password,	and	the	PasswordChangeDoneView	view	will	display	a success
    message	after	the	user	has	successfully	changed	his	password.

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,16 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    
+    #change password urls (using Django authentication views)
+    #PasswordChangeView will handle the form to change the password
+    path('password_change/', auth_views.PasswordChangeView.as_view(),name='password_change'),
+    # will display a success message for password changed
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    #reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
