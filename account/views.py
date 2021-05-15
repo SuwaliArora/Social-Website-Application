5105cda20694fcbfef2674a3cad30a8162179fc4
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-15 13:50:50 +0530

    Updated views.py
    We	use	the	login_required	decorator	because	users	have	to	be
    authenticated	to	edit	their	profile.	In	this	case,	we	are	using	two
    model	forms:	UserEditForm	to	store	the	data	of	the	built-in	user	model
    and	ProfileEditForm	to	store	the	additional	profile	data	in	the
    custom	Profile	model.	To	validate	the	submitted	data,	we	will
    execute	the	is_valid()	method	of	both	forms.	If	both	forms	contain
    valid	data,	we	will	save	both	forms,	calling	the	save()	method	to
    update	the	corresponding	objects	in	the	database.

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
 def user_login(request):  #user login view is called with a get request
@@ -42,8 +43,23 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             #save the user object
             new_user.save()
+            Profile.objects.create(user=new_user)  #create the user profile
             return render(request, 'account/register_done.html', {'new_user': new_user})
 
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
+
+@login_required  #login_required decorator because users have to be authenticated to edit their	profile.
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
