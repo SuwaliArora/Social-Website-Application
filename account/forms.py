d87d89a57c243f937f642da051c06c1c0e133c82
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-15 13:54:05 +0530

    Updated forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,14 +1,16 @@
 from django import forms
+from django.db import models
 from django.forms import fields
 from django.contrib.auth.models import User
+from .models import Profile
 
-#login form
+#login form using Django authentication framework
 #this form willbe used to authenticate users against databse
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     #passwordInput widget to render its html input element
-    
+
 #sign up form to allow user registration on website
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
@@ -24,3 +26,13 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2'] :
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+class UserEditForm(forms.ModelForm): #to edit their profile, which r attributes of built-in Django user model
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm): #to edit their profile, which r attributes of built-in custom profile model
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
