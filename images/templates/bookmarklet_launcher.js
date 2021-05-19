61fa380fec75492678c057abafbedfd6cf4b68e3
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-19 13:29:51 +0530

    Added bookmarklet_launcher.js
    This is the launcher script. 
    This script discovers whether the bookmarklet has been already loaded by checking whether the myBookmarklet variable is defined.

---
+++ images/templates/bookmarklet_launcher.js
@@ -0,0 +1,12 @@
+/* a bookmarklet is bookmark stored in web browser that contains JS code to extend browser's functionality */
+// for bookmarklet
+
+(function(){
+    if (window.myBookmarklet !== undefined){
+        myBookmarklet();
+    }
+    else{
+        document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+
+    }
+})();
