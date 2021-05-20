5f62c3cdc50a7be92f880d11d98c4670903b370f
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-20 17:26:24 +0530

    Updated bookmarklet.js

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,7 +1,7 @@
 //jQuery loader script
 (function(){
     var jquery_version = '3.3.1';    // jquery version to load
-    var site_url = 'http://127.0.0.1:8000/';   //the base url for our website
+    var site_url = 'http://3f6ad53c.ngrok.io/';   //the base url for our website
     var static_url = site_url + 'static/';   //base static files url
     var min_width = 100;   //min width & height in px for images
     var min_height = 100;
@@ -41,3 +41,33 @@ else{
         })();
     }
 })()
+
+function bookmarklet(msg)
+{
+    //load Css
+    var css = jQuery('<link>');
+    css.attr({
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: static_url + 'css/bookmarklet.css?r=' + Math.floor(Math.random()*99999999999999999999)
+    });
+    jQuery('head').append(css);
+
+    //load HTML
+    box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a><h1>Select an image to bookmark:</h1><div class="iamges"></div></div>';
+    jQuery('body').append(box_html);
+
+    //close event- removes html from doc when user click close link
+    jQuery('#bookmarklet #close').click(function(){
+        jQuery('#bookmarklet').remove();
+    });
+};
+
+//find images and display them
+jQuery.each(jQuery('img[src$="jpg"]'), function(index, image){
+    if (jQuery(image).width() >= min_width && jQuery(image).height()>=min_height)
+    {
+        image_url = jQuery(image).attr('src');
+        jQuery('#bookmarklet .images').append('<a href="#"><img src="'+ image_url +'" /></a>');
+    }
+});
