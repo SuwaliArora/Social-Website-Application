5013ee5e8c4642b01d5a79ccd0c42db85c6ea4b2
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-22 16:25:29 +0530

    Updated setting.py
    The	sorl-thumbnail	application	offers	you	different	ways	to	define
    image	thumbnails.	The	application	provides	a	{%	thumbnail	%}	template
    tag	to	generate	thumbnails	in	templates	and	a	custom	ImageField	if
    you	want	to	define	thumbnails	in	your	models.	We	will	use	the
    template	tag	approach.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',   
     'social_django',
     'images.apps.ImagesConfig',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [ 	#middleware	is a class with methods	that are globally executed during the request or response phase.	
