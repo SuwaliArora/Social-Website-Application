44f740eb2497fe35ef55439008420f302e308d27
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-05-28 20:11:56 +0530

    Update models.py
    Created Contact model, an intermediary model to build relationships between	users. There are two reasons why we want to use	an intermediate model:
    1. We are using the User model provided by Django, and we want to	avoid altering	it.
    2. We want to	store the	time	when the	relation	is created.

--- account/models.py
+++ account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.fields import related
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
@@ -12,3 +14,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+class Contact(models.Model):
+    #user_from - for user that creates relationship,
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    #user_to - for the user being followed
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    #to store the time when relationship was created
+    created = models.DateTimeField(auto_now_add=True, db_index=True)    #db_index is true to create db index for created field
+   
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
+
+following = models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False)
+#This	is	a	many-to-many	relationship	from	the	User model	to	itself:	we	refer	to	'self'	in	the	ManyToManyField	field	to	create	a
+# relationship	to	the	same	model.
+
+#add following field to user dynamically ,  symmetrical=False to define	a	non-symmetric relation.
+User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
+    
